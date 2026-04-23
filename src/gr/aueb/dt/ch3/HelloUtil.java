f8b928e22ef57befaa28ed4e737eb58ed81bfc50
a8anassis <a8anassis@gmail.com>
2026-04-23 17:27:03 +0300

    feat(hello): Add HelloUtil class with a method to print greeting

---
+++ src/gr/aueb/dt/ch3/HelloUtil.java
@@ -0,0 +1,14 @@
+package gr.aueb.dt.ch3;
+
+public class HelloUtil {
+
+    /**
+     * No instances of this class should be available.
+     */
+    private HelloUtil() {
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello, Digital Transformation!");
+    }
+}
