535b1ce791cf3b68ff4ae6f6fb36c88acdb20227
a8anassis <a8anassis@gmail.com>
2026-04-23 17:08:24 +0300

    chore(account): Implement main method to demonstrate Account operations

---
+++ src/gr/aueb/dt/ch2/Main.java
@@ -0,0 +1,16 @@
+package gr.aueb.dt.ch2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Account aliceAccount = new Account(1, "GR1234", "Alice", "Smith", "AB12345", 1000.0);
+
+        try {
+            aliceAccount.deposit(500.0);
+            aliceAccount.withdraw(1000.0, "GR1234");
+            System.out.println("Alice's balance is " + aliceAccount.getBalance());
+        } catch (Exception e) {
+            System.out.println("Oops! An error occurred: ");
+        }
+    }
+}
