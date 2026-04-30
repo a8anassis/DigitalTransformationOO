f45394ba247674873972eb976b53c6ac09b830ab
a8anassis <a8anassis@gmail.com>
2026-04-30 18:01:44 +0300

    feat(generic): Add GenericNode class for generic node implementation

---
+++ src/gr/aueb/dt/ch7/Main.java
@@ -0,0 +1,9 @@
+package gr.aueb.dt.ch7;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GenericNode<String> strNode = new GenericNode<>();
+        GenericNode<Double> doubleNode = new GenericNode<>();
+    }
+}
